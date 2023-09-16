8f24c5e10edb686c836f9f0060a839c318c65ad9
xordions <125524859+XXordions@users.noreply.github.com>
2023-09-16 22:06:04 +0300

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,83 @@
+
+whois = [
+  "<br>",
+  "16 yaşındayım, süperim 🤗",
+  "Antalya'da yaşıyorum ve lise öğrencisiyim",
+  "Şuana kadar bir çok dilde bir çok bot ve site kodladım",
+  "Az çok bildiğim diller: Js, Lua, Python",
+  "<br>"
+];
+
+
+var github = "https://github.com/XXordions";
+var insta = "https://www.instagram.com/xordions/";
+var youtubee = "https://www.youtube.com/channel/UCoceLDZ7RGfBEfZeJ3HMmuw"
+var spo = "https://open.spotify.com/user/9of2aeqhxm3zzpx2awhw9ahhn?si=2cc30ed1f62d43ee"
+social = [
+  "<br>",
+  'Youtube        <a href="' + youtubee + '" target="_blank">XORDIONS' + "</a>",
+  'Instagram      <a href="' + insta + '" target="_blank">instagram/xordions' + '</a>',
+  'Spotify        <a href="'+spo+'"target="_blank">spotify/XORDIONS',   
+  'Github         <a href="' + github + '" target="_blank">github/Xordions' + "</a>",   
+  "<br>"
+];
+
+
+
+
+var sarki = 
+[
+  "https://www.youtube.com/embed/dPgj93skycA?autoplay=1",
+  "https://www.youtube.com/embed/5JmQIQbtJpU?autoplay=1",
+  "https://www.youtube.com/embed/sOeD_ZHfvww?autoplay=1",
+  "https://www.youtube.com/embed/9XCqST1aSf8?autoplay=1",
+  "https://www.youtube.com/embed/xu9xVNuE-_0?autoplay=1",
+  "https://www.youtube.com/embed/fq3d2y4x_C0?autoplay=1",
+  "https://www.youtube.com/embed/mSHz6cZqjNM?autoplay=1",
+  "https://www.youtube.com/embed/EFOHkWLpf_M?autoplay=1",
+  "https://www.youtube.com/embed/_L1YNZp00s8?autoplay=1",
+  "https://www.youtube.com/embed/hurSH3n_U8E?autoplay=1",
+  "https://www.youtube.com/embed/TDSgb2P76fc?autoplay=1",
+  "https://www.youtube.com/embed/Uj0djfOlvkQ?autoplay=1",
+  "https://www.youtube.com/embed/pgbgmHx9GLs?autoplay=1",
+  "https://www.youtube.com/embed/TjO15KuKKCc?autoplay=1",
+  "https://www.youtube.com/embed/Fyb4QAZVVv0?autoplay=1",
+  "https://www.youtube.com/embed/V3lmf_KIAOU?autoplay=1",
+]
+
+
+
+
+
+help = [
+  "<br>",
+  '<span class="command">Command:</span>         Just write "whois" or "social".',
+  '<span class="command">whois</span>    =>      Kim Bu Xordi 😁?',
+  '<span class="command">social</span>   =>      Sosyal Medya Hesaplarım',
+  "<br>",
+];
+
+
+
+
+
+banner2 = [
+  "XXXXXXX       XXXXXXX          OOOOOOOOO          RRRRRRRRRRRRRRRRR        DDDDDDDDDDDDD             IIIIIIIIII",
+  "X:::::X       X:::::X        OO:::::::::OO        R::::::::::::::::R       D::::::::::::DDD          I::::::::I",
+  "X:::::X       X:::::X      OO:::::::::::::OO      R::::::RRRRRR:::::R      D:::::::::::::::DD        I::::::::I" ,
+  "X::::::X     X::::::X     O:::::::OOO:::::::O     RR:::::R     R:::::R     DDD:::::DDDDD:::::D       II::::::II ",
+  "XXX:::::X   X:::::XXX     O::::::O   O::::::O       R::::R     R:::::R       D:::::D    D:::::D        I::::I  " ,
+  "   X:::::X X:::::X        O:::::O     O:::::O       R::::R     R:::::R       D:::::D     D:::::D       I::::I  ",
+  "    X:::::X:::::X         O:::::O     O:::::O       R::::RRRRRR:::::R        D:::::D     D:::::D       I::::I  ",
+  "     X:::::::::X          O:::::O     O:::::O       R:::::::::::::RR         D:::::D     D:::::D       I::::I  ",
+  "     X:::::::::X          O:::::O     O:::::O       R::::RRRRRR:::::R        D:::::D     D:::::D       I::::I  ",
+  "    X:::::X:::::X         O:::::O     O:::::O       R::::R     R:::::R       D:::::D     D:::::D       I::::I  " ,
+  "   X:::::X X:::::X        O:::::O     O:::::O       R::::R     R:::::R       D:::::D     D:::::D       I::::I  " ,
+  "XXX:::::X   X:::::XXX     O::::::O   O::::::O       R::::R     R:::::R       D:::::D    D:::::D        I::::I  ",
+  "X::::::X     X::::::X     O:::::::OOO:::::::O     RR:::::R     R:::::R     DDD:::::DDDDD:::::D       II::::::II",
+  "X:::::X       X:::::X      OO:::::::::::::OO      R::::::R     R:::::R     D:::::::::::::::DD        I::::::::I",
+  "X:::::X       X:::::X        OO:::::::::OO        R::::::R     R:::::R     D::::::::::::DDD          I::::::::I",
+  "XXXXXXX       XXXXXXX          OOOOOOOOO          RRRRRRRR     RRRRRRR     DDDDDDDDDDDDD             IIIIIIIIII",
+
+"<span class=\"color2\">Komutları Görmek Için</span> <span class=\"command\">'help'</span><span class=\"color2\"> yazınız.</span>",
+]
